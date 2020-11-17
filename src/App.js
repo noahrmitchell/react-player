4fa4d1a279706ac292bdc78a375b7dc747655b04
noahrmitchell <noahrmitchell@gmail.com>
2020-11-17 21:52:23 +0000

    Created library panel on left and applied initial stlying

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 //Styles
 import './styles/app.scss'
 //Components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library'
+import data from './Data';
 
 function App() {
+  //State 
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <h1>Music Player</h1>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player 
+        currentSong={currentSong} 
+        isPlaying={isPlaying} 
+        setIsPlaying={setIsPlaying}/>
+      <Library songs={songs} setCurrentSong={setCurrentSong}/>
     </div>
   );
 }
