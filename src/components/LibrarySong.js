14ae1e63c196c2d6938064fea723e869ae9bc692
noahrmitchell <noahrmitchell@gmail.com>
2020-11-17 22:18:42 +0000

    Updated player to play songs when clicked

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function LibrarySong({ song, songs, setCurrentSong, id }) {  
+    //Handlers
+    const songSelectHandler = () => {
+        setCurrentSong(song);
+    }  
+    return (
+        <div onClick={songSelectHandler} className="library-song">
+            <img src={song.cover} alt={song.name}></img>
+            <div className="song-description">
+                <h3>{song.name}</h3>  
+                <h4>{song.artist}</h4>  
+            </div> 
+        </div>
+    )
+}
+
+export default LibrarySong; 
